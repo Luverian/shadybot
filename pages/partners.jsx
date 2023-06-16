1f53ecab53c075d1cf8aea475c7f93dd6c33743c
Luverian <134546522+Luverian@users.noreply.github.com>
2023-06-16 23:52:20 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -16,14 +16,14 @@ export default function Partners() {
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://discord.gg/6XGqdgE">
+            <a href="https://discord.gg/JaJzH3M7xv">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/676124992919830548/68ef946cf7cc39e4063eed4d446f6dda.webp" />
+                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1111328614575788143/1119368150098653184/pngwing.com.png?width=676&height=676" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Code Share</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                        Share Your Codes, Can You Keep Your Projects Active Forever. World's Largest Website with Discord Interface!
+                                        Waiting...
                                     </p>
                                 </div>     
                             </div>
@@ -32,14 +32,14 @@ export default function Partners() {
                     </div>
 
                     <div className="flex flex-col justify-center text-white rounded">
-                        <a href="https://discord.gg/Fy4WmSThRZ">
+                        <a href="https://discord.gg/JaJzH3M7xv">
                             <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/924312493537509407/721de16575a9048c9c6980613ebb99f0.webp" />
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/1111292586750136320/e02fa2b774b8cca1c1fcc29385f82abc.png" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">The Developers</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                    You can get help on software and similar issues or you can spend time chatting. How about being a cool developer?
+                                    ShadyBOT Support Server.
                                     </p>
                                 </div>     
                             </div>
